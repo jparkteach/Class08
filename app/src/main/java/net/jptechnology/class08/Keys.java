05a303a3d10fa31464f43249b4939b39d740ab55
John <jparkteach@yahoo.com>
2017-03-06 20:38:21 -0500

    New Project

---
+++ app/src/main/java/net/jptechnology/class08/Keys.java
@@ -0,0 +1,5 @@
+package net.jptechnology.class08;
+
+public interface Keys {
+    String PERSON = "person";
+}
---
+++ app/src/main/java/net/jptechnology/class08/SecondActivity.java
@@ -0,0 +1,23 @@
+package net.jptechnology.class08;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+public class SecondActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+    }
+
+    public void submitPerson(View view) {
+        Person person = new Person("Xi", 22);
+        Intent data = new Intent();
+        data.putExtra(Keys.PERSON, person);
+        setResult(RESULT_OK, data);
+        finish();
+    }
+}
